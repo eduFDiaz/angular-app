954041612fe9a9d07d937abfa0866b1cea08e960
Eduardo Antonio Fernandez Diaz <fernandez9000@gmail.com>
2019-08-11 16:45:11 -0500

    event binding used from forms

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  
+  constructor() { 
+    setTimeout(() => {
+        this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'A server was created!';;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
